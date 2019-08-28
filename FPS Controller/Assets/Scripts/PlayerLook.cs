3777df2f5199190eeff17d69fb9fb414eef4043d
andtarspb <46713402+andtarspb@users.noreply.github.com>
2019-08-28 20:53:13 +0300

    basic movement + camera controll

---
+++ FPS Controller/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    // PRIVATE INIT     
+    [SerializeField]
+    private string mouseXInputName, mouseYInputName;
+    [SerializeField]
+    private float mouseSensitivity;
+
+    private float xAxisClamp;
+
+    [SerializeField]
+    private Transform playerBody;
+
+    private void Awake()
+    {
+        LockCursor();
+        xAxisClamp = 0.0f;
+    }
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        // получаем входные данные
+        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+        // ограничиваем вращение по y
+        xAxisClamp += mouseY;
+
+        if (xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90;
+            mouseY = 0f;
+            ClampXAxisRotationToValue(270);
+        } else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90;
+            mouseY = 0f;
+            ClampXAxisRotationToValue(90);
+
+        }
+
+        // вращаем камеру
+        transform.Rotate(Vector3.left * mouseY);
+
+        // вращаем игрока
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
---
+++ FPS Controller/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    private string horizontalInputName;
+    [SerializeField]
+    private string verticalInputName;
+    [SerializeField]
+    private float movementSpeed;
+
+    private CharacterController charController;
+
+    [SerializeField]
+    private AnimationCurve jumpFallOff;
+    [SerializeField]
+    private float jumpMultiplier;
+    [SerializeField]
+    private KeyCode jumpKey;
+
+    private bool isJumping;
+
+    private float initSlopeLimit;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+        initSlopeLimit = charController.slopeLimit;
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        // получаем входные данные
+        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
+
+        // calculating movement
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        // perfoming a movement
+        charController.SimpleMove(forwardMovement + rightMovement);
+
+        JumpInput();
+    }
+
+    private void JumpInput()
+    {
+        if (Input.GetKeyDown(jumpKey) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    private IEnumerator JumpEvent()
+    {
+        charController.slopeLimit = 90;
+        float timeInTheAir = 0;
+
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(timeInTheAir);
+            charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInTheAir += Time.deltaTime;
+
+            yield return null;
+        }
+        while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+
+        charController.slopeLimit = initSlopeLimit;
+        isJumping = false;
+    }
+}
